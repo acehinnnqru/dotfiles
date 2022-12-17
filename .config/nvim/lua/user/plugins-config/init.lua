a39beafc74c27610e499da8b8edd7f2fadc2c162
acehinnnqru <acehinnnqru@gmail.com>
2022-12-17 20:30:51 +0800

    feat: update plugins, options

--- .config/nvim/lua/user/plugins-config/init.lua
+++ .config/nvim/lua/user/plugins-config/init.lua
@@ -14,8 +14,10 @@ vim.opt.listchars:append "space:⋅"
 vim.opt.listchars:append "eol:↴"
 
 require("indent_blankline").setup {
-    show_end_of_line = true,
+    show_end_of_line = false,
     space_char_blankline = " ",
+    show_current_context = true,
+    show_current_context_start = true,
 }
 
 vim.o.foldcolumn = '0' -- '0' is not bad

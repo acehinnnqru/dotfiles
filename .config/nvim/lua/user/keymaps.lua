c2482e5b9b8f199c6ac6da5bc7377be0d06d9687
acehinnnqru <acehinnnqru@gmail.com>
2022-12-21 18:25:05 +0800

    feat(yabai): update yabairc

--- .config/nvim/lua/user/keymaps.lua
+++ .config/nvim/lua/user/keymaps.lua
@@ -20,7 +20,7 @@ keymap("n", "<leader>bn", ":bnext<CR>", opts)
 
 -- telescope plugin keymaps
 keymap("n", "<leader>ff", "<cmd>lua require('telescope.builtin').find_files()<cr>", opts)
-keymap("n", "<leader>fa", "<cmd>lua require('telescope.builtin').find_files({hidden=true})<cr>", opts)
+keymap("n", "<leader>fa", "<cmd>lua require('telescope.builtin').find_files({hidden=true,ignore=true})<cr>", opts)
 keymap("n", "<leader>fg", "<cmd>lua require('telescope.builtin').live_grep()<cr>", opts)
 keymap("n", "<leader>fb", "<cmd>lua require('telescope.builtin').buffers()<cr>", opts)
 keymap("n", "<leader>fh", "<cmd>lua require('telescope.builtin').help_tags()<cr>", opts)

9bc6a84b376afd89b087a1623e5020ae6a867b26
acehinnnqru <acehinnnqru@gmail.com>
2022-12-27 14:16:23 +0800

    feat(nvim)!: add lazy.nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,21 @@
+-- boostrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "--single-branch",
+    "https://github.com/folke/lazy.nvim.git",
+    lazypath,
+  })
+end
+
+vim.opt.runtimepath:prepend(lazypath)
+
+-- load lazy.nvim
+require("lazy").setup("plugins")
+
 require "user.options"
 require "user.plugins"
 require "user.keymaps"

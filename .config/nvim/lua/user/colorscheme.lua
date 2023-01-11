e42a16fa259b86757d78a9fa673de3064baa10ec
acehinnnqru <acehinnnqru@gmail.com>
2023-01-11 21:12:55 +0800

    feat(nvim): remove old configs

--- .config/nvim/lua/user/colorscheme.lua
+++
@@ -1,9 +0,0 @@
-require("github-theme").setup({
-    theme_style = 'dark',
-    comment_style = "italic",
-    keyword_style = "italic",
-    function_style = "italic",
-    variable_style = "italic",
-    dark_sidebar = false,
-    transparent = true,
-})

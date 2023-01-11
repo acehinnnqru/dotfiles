a66040c93001de0c7b1127d9ff175301349ff275
acehinnnqru <acehinnnqru@gmail.com>
2023-01-11 23:39:45 +0800

    feat(nvim): add ui plugins

---
+++ .config/nvim/lua/plugins/ui.lua
@@ -0,0 +1,86 @@
+return {
+	{
+		"xiyaowong/nvim-transparent",
+		event = "VimEnter",
+		config = function()
+			require("transparent").setup({
+				enable = true,
+			})
+		end,
+	},
+
+	-- bufferline
+	{
+		"akinsho/bufferline.nvim",
+		event = "VeryLazy",
+		opts = {
+			options = {
+				diagnostics = "nvim_lsp",
+				show_tab_indicators = true,
+				show_close_icon = false,
+			}
+		}
+	},
+	-- status line
+	{
+
+		"nvim-lualine/lualine.nvim",
+		event = "VeryLazy", 
+
+		opts = {
+			sections = {
+				lualine_x = {
+					{
+						require("lazy.status").updates,
+						cond = require("lazy.status").has_updates,
+						color = { fg = "#ff9e64" },
+					},
+				},
+			},
+		}
+
+
+	},
+
+	-- welcome page
+	{
+		"goolord/alpha-nvim",
+		event = "VimEnter",
+		config = function()
+			local alpha = require("alpha")
+			local dashboard = require("alpha.themes.dashboard")
+			local logo = {
+				[[███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗]],
+				[[████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║]],
+				[[██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║]],
+				[[██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║]],
+				[[██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║]],
+				[[╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝]],
+			}
+			dashboard.section.header.val = logo
+			dashboard.section.buttons.val = {
+				dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
+				dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
+				dashboard.button("p", "  Find project", ":Telescope projects <CR>"),
+				dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
+				dashboard.button("t", "  Find text", ":Telescope live_grep <CR>"),
+				dashboard.button("c", "  Configuration", ":e ~/.config/nvim/init.lua <CR>"),
+				dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
+			}
+
+			local function footer()
+				return "acehinnnqru"
+			end
+
+			dashboard.section.footer.val = footer()
+
+			dashboard.section.footer.opts.hl = "Type"
+			dashboard.section.header.opts.hl = "Include"
+			dashboard.section.buttons.opts.hl = "Keyword"
+
+			dashboard.opts.opts.noautocmd = true
+
+			alpha.setup(dashboard.opts)
+		end,
+	}
+}

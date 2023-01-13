856732751885d7085c47a5648c45f8d5b80ed66b
acehinnnqru <acehinnnqru@gmail.com>
2023-01-13 17:24:52 +0800

    feat(nvim): configure lsp ui

--- .config/nvim/lua/plugins/lsp/init.lua
+++ .config/nvim/lua/plugins/lsp/init.lua
@@ -1,86 +1,106 @@
 return {
-    -- lsp config
-    {
-        "neovim/nvim-lspconfig",
-        event = "BufReadPre",
-        dependencies = {
-            { "folke/neodev.nvim", opts = { experimental = { pathStrict = true } } },
-            "mason.nvim",
-            "williamboman/mason-lspconfig.nvim",
-            "hrsh7th/cmp-nvim-lsp",
-        },
-        opts = {
-            servers = {},
-            setup = {},
-        },
-        config = function(_, opts)
-            -- setup formatting and keymaps
-            require("plugins.lsp.apis").on_attach(function(client, buffer)
-                require("plugins.lsp.format").on_attach(client, buffer)
-                require("plugins.lsp.keymaps").on_attach(client, buffer)
-            end)
+	-- lsp config
+	{
+		"neovim/nvim-lspconfig",
+		event = "BufReadPre",
+		dependencies = {
+			{ "folke/neodev.nvim", opts = { experimental = { pathStrict = true } } },
+			"mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
+			"hrsh7th/cmp-nvim-lsp",
+		},
+		opts = {
+			servers = {},
+			setup = {},
+		},
+		config = function(_, opts)
+			-- setup formatting and keymaps
+			require("plugins.lsp.apis").on_attach(function(client, buffer)
+				require("plugins.lsp.format").on_attach(client, buffer)
+				require("plugins.lsp.keymaps").on_attach(client, buffer)
+			end)
 
-            -- diagnostics
-            vim.diagnostic.config({
-                underline = true,
-                update_in_insert = false,
-                virtual_text = { spacing = 4, prefix = "●" },
-                severity_sort = true,
-            })
+			-- diagnostics
+			vim.diagnostic.config({
+				underline = true,
+				update_in_insert = false,
+				virtual_text = { spacing = 4, prefix = "●" },
+				severity_sort = true,
+			})
 
-            local servers = opts.servers
-            local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
+			local servers = opts.servers
+			local capabilities =
+				require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
-            require("mason-lspconfig").setup({ ensure_installed = vim.tbl_keys(servers) })
-            require("mason-lspconfig").setup_handlers({
-                function(server)
-                    local server_opts = servers[server] or {}
-                    server_opts.capabilities = capabilities
-                    if opts.setup[server] then
-                        if opts.setup[server](server, server_opts) then
-                            return
-                        end
-                    elseif opts.setup["*"] then
-                        if opts.setup["*"](server, server_opts) then
-                            return
-                        end
-                    end
-                    require("lspconfig")[server].setup(server_opts)
-                end,
-            })
-        end
-    },
+			require("mason-lspconfig").setup({ ensure_installed = vim.tbl_keys(servers) })
+			require("mason-lspconfig").setup_handlers({
+				function(server)
+					local server_opts = servers[server] or {}
+					server_opts.capabilities = capabilities
+					if opts.setup[server] then
+						if opts.setup[server](server, server_opts) then
+							return
+						end
+					elseif opts.setup["*"] then
+						if opts.setup["*"](server, server_opts) then
+							return
+						end
+					end
+					require("lspconfig")[server].setup(server_opts)
+				end,
+			})
+		end,
+	},
 
-    -- cmdline tools and lsp servers
-    {
-        "williamboman/mason.nvim",
-        cmd = "Mason",
-        keys = { { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" } },
-        opts = {
-            ensure_installed = {
-                "stylua"
-            },
-        },
-        ---@param opts MasonSettings | {ensure_installed: string[]}
-        config = function(_, opts)
-            require("mason").setup(opts)
-            local mr = require("mason-registry")
-            for _, tool in ipairs(opts.ensure_installed) do
-                local p = mr.get_package(tool)
-                if not p:is_installed() then
-                    p:install()
-                end
-            end
-        end,
-    },
+	-- lsp signiture
+	{
+		"ray-x/lsp_signature.nvim",
+        dependencies = { "neovim/nvim-lspconfig" },
+		config = function()
+			-- handlers
+			vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
+			vim.lsp.handlers["textDocument/signatureHelp"] =
+				vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" })
 
-    -- formatters
-    {
-        "jose-elias-alvarez/null-ls.nvim",
-        event = "BufReadPre",
-        dependencies = { "mason.nvim" },
-        opts = {
-            sources = {},
-        },
-    },
+			-- lsp signature configs
+			local signature_config = {
+				bind = true,
+				handler_opts = { border = "rounded" },
+			}
+			require("lsp_signature").setup(signature_config)
+		end,
+	},
+
+	-- cmdline tools and lsp servers
+	{
+		"williamboman/mason.nvim",
+		cmd = "Mason",
+		keys = { { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" } },
+		opts = {
+			ensure_installed = {
+				"stylua",
+			},
+		},
+		---@param opts MasonSettings | {ensure_installed: string[]}
+		config = function(_, opts)
+			require("mason").setup(opts)
+			local mr = require("mason-registry")
+			for _, tool in ipairs(opts.ensure_installed) do
+				local p = mr.get_package(tool)
+				if not p:is_installed() then
+					p:install()
+				end
+			end
+		end,
+	},
+
+	-- formatters
+	{
+		"jose-elias-alvarez/null-ls.nvim",
+		event = "BufReadPre",
+		dependencies = { "mason.nvim" },
+		opts = {
+			sources = {},
+		},
+	},
 }

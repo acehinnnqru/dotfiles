24fc419cc1a1ff8ca81328346cb0badce4b949c2
acehinnnqru <acehinnnqru@gmail.com>
2023-01-13 10:42:19 +0800

    feat(nvim): add lua lsp

---
+++ .config/nvim/lua/plugins/extras/languages/lua.lua
@@ -0,0 +1,53 @@
+return {
+	-- modified treesitter config
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = function(_, opts)
+			vim.list_extend(opts.ensure_installed, { "lua" })
+		end,
+	},
+
+	{
+		"williamboman/mason.nvim",
+		opts = function(_, opts)
+			table.insert(opts.ensure_installed, "stylua")
+			return opts
+		end,
+	},
+
+	{
+		"jose-elias-alvarez/null-ls.nvim",
+		opts = function(_, opts)
+			local nls = require("null-ls")
+			table.insert(opts.sources, nls.builtins.formatting.stylua)
+		end,
+	},
+
+	-- correctly setup lspconfig
+	{
+		"neovim/nvim-lspconfig",
+		opts = {
+			-- make sure mason installs the server
+			servers = {
+				sumneko_lua = {
+					settings = {
+						Lua = {
+							runtime = {
+								version = "LuaJIT",
+							},
+							diagnostics = {
+								globals = { "vim" },
+							},
+							workspace = {
+								library = vim.api.nvim_get_runtime_file("", true),
+							},
+							telemetry = {
+								enable = false,
+							},
+						},
+					},
+				},
+			},
+		},
+	},
+}

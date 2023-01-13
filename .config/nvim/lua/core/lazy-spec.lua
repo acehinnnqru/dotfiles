d422583aaef6d38cd9c79c95397b67887505aa5b
acehinnnqru <acehinnnqru@gmail.com>
2023-01-13 16:44:37 +0800

    feat(nvim): add lazy spec

---
+++ .config/nvim/lua/core/lazy-spec.lua
@@ -0,0 +1,9 @@
+return {
+    spec = {
+        { import = "plugins" },
+        { import = "plugins.extras.languages.lua" },
+        { import = "plugins.extras.languages.json" },
+        { import = "plugins.extras.languages.rust" },
+    },
+}
+

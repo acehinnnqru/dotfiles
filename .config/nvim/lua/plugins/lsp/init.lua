f05486b8a1b0d298ecb08b65ab73c72f54d8ee66
acehinnnqru <acehinnnqru@gmail.com>
2023-01-19 03:26:43 +0800

    feat(nvim): update lsp config

--- .config/nvim/lua/plugins/lsp/init.lua
+++ .config/nvim/lua/plugins/lsp/init.lua
@@ -7,7 +7,6 @@ return {
 			{ "folke/neodev.nvim", opts = { experimental = { pathStrict = true } } },
 			"mason.nvim",
 			"williamboman/mason-lspconfig.nvim",
-			"hrsh7th/cmp-nvim-lsp",
 		},
 		opts = {
 			servers = {},

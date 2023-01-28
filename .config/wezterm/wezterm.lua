d94645d12d70d2d03d6836487fdfbc37a17cb60d
acehinnnqru <acehinnnqru@gmail.com>
2023-01-28 13:41:14 +0800

    feat(wezterm): update wezterm config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,19 +1,21 @@
 local wezterm = require("wezterm")
+
 return {
 	color_scheme = "Catppuccin Macchiato",
 	font = wezterm.font_with_fallback({
 		{
-			family = "Consolas",
+			family = "Cascadia Code",
 			style = "Italic",
 		},
+
 		{
 			family = "Hack Nerd Font Mono",
 		},
 	}),
-	font_size = 13.0,
+	font_size = 13.5,
 
 	window_decorations = "RESIZE",
 
 	hide_tab_bar_if_only_one_tab = true,
-	window_background_opacity = 0.75,
+	window_background_opacity = 0.95,
 }

b24ef0a1cc22de6f97584670624517dceccb2952
acehinnnqru <acehinnnqru@gmail.com>
2023-02-04 01:34:25 +0800

    feat(nvim): add super-tab support

--- .config/nvim/lua/plugins/coding/nvim-cmp.lua
+++ .config/nvim/lua/plugins/coding/nvim-cmp.lua
@@ -1,3 +1,9 @@
+local has_words_before = function()
+  unpack = unpack or table.unpack
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
 return {
 	"hrsh7th/nvim-cmp",
 	event = { "InsertEnter", "CmdlineEnter" },
@@ -57,17 +63,39 @@ return {
 				["<C-f>"] = cmp.mapping.scroll_docs(4),
 				["<C-e>"] = cmp.mapping.abort(),
 				["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+				["<Tab>"] = cmp.mapping(function(fallback)
+                    local luasnip = require("luasnip")
+					if cmp.visible() then
+						cmp.select_next_item()
+					elseif luasnip.expand_or_locally_jumpable() then
+						luasnip.expand_or_jump()
+					elseif has_words_before() then
+						cmp.complete()
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+
+				["<S-Tab>"] = cmp.mapping(function(fallback)
+                    local luasnip = require("luasnip")
+					if cmp.visible() then
+						cmp.select_prev_item()
+					elseif luasnip.jumpable(-1) then
+						luasnip.jump(-1)
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
 			}),
 			sources = cmp.config.sources({
-				{ name = "nvim_lsp" },
-				{ name = "lsp_signature" },
-				{ name = "luasnip" },
+				{ name = "nvim_lsp", keyword_length = 2 },
+				{ name = "lsp_signature", keyword_length = 2 },
+				{ name = "luasnip", keyword_length = 1 },
 			}, {
-				{ name = "nvim_lsp_signature_help" },
+				{ name = "nvim_lsp_signature_help", keyword_length = 3 },
 			}, {
-
-				{ name = "buffer" },
 				{ name = "path" },
+				{ name = "buffer", keyword_length = 3 },
 			}),
 			experimental = {
 				ghost_text = {

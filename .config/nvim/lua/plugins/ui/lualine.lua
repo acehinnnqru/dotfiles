1b4af677f701c45ee15b836dc74fd3351e0596cb
acehinnnqru <acehinnnqru@gmail.com>
2023-02-21 10:44:15 +0800

    feat(nvim): update lualine

--- .config/nvim/lua/plugins/ui/lualine.lua
+++ .config/nvim/lua/plugins/ui/lualine.lua
@@ -5,6 +5,8 @@ return {
 	opts = {
 		options = {
 			disabled_filetypes = { statusline = { "dashboard", "lazy", "alpha" } },
+			section_separators = { left = "", right = "" },
+			component_separators = { left = "", right = "" },
 		},
 	},
 }

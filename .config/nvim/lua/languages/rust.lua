376d624686d80d277feb0555a0f10a0fe35d7294
acehinnnqru <acehinnnqru@gmail.com>
2023-03-19 03:04:10 +0800

    feat(nvim): add dap support

--- .config/nvim/lua/languages/rust.lua
+++ .config/nvim/lua/languages/rust.lua
@@ -18,8 +18,6 @@ return {
 		end,
 	},
 
-	{ "simrat39/rust-tools.nvim", lazy = true, ft = { "rs", "rust" } },
-
 	-- modified treesitter config
 	{
 		"nvim-treesitter/nvim-treesitter",
@@ -32,66 +30,87 @@ return {
 		"williamboman/mason.nvim",
 		opts = function(_, opts)
 			table.insert(opts.ensure_installed, "rust-analyzer")
+			table.insert(opts.ensure_installed, "codelldb")
 			return opts
 		end,
 	},
 
 	{
 		"neovim/nvim-lspconfig",
+		dependencies = { "simrat39/rust-tools.nvim" },
 		opts = {
 			servers = {
 				rust_analyzer = {},
 			},
-		},
-		setup = {
-			rust_analyzer = function(_, opts)
-				require("plugins.lsp.apis").on_attach(function(client, buffer)
-					if client.name == "rust_analyzer" then
-						local rt = require("rust-tools")
+			setup = {
+				rust_analyzer = function(_, opts)
+					require("plugins.lsp.apis").on_attach(function(client, buffer)
+						if client.name == "rust_analyzer" then
+							local rt = require("rust-tools")
                             -- stylua: ignore start
-							vim.keymap.set( "n", "K", rt.hover_actions.hover_actions, { desc = "Hover Action", buffer = buffer })
-							vim.keymap.set( "n", "<leader>cem", rt.expand_macro.expand_macro, { desc = "Expand Macro", buffer = buffer })
-							vim.keymap.set( "n", "<leader>cpm", rt.parent_module.parent_module, { desc = "Parent Module", buffer = buffer })
-							vim.keymap.set( "n", "<leader>cmd", "RustMoveItemDown", { desc = "Move Item Down", buffer = buffer })
-							vim.keymap.set( "n", "<leader>cmu", "RustMoveItemUp", { desc = "Move Item Up", buffer = buffer })
-							vim.keymap.set( "n", "<leader>cjl", rt.join_lines.join_lines, { desc = "Join Lines", buffer = buffer })
-							vim.keymap.set( "n", "<leader>che", rt.inlay_hints.enable, { desc = "Inlay Hints Enable", buffer = buffer })
-							vim.keymap.set( "n", "<leader>chd", rt.inlay_hints.disable, { desc = "Inlay Hints Disable", buffer = buffer })
-						-- stylua: ignore end
-					end
-				end)
+                            vim.keymap.set( "n", "K", "<cmd>RustHoverActions<cr>", { desc = "Hover Action", buffer = buffer })
+                            vim.keymap.set( "n", "<leader>cem", rt.expand_macro.expand_macro, { desc = "Expand Macro", buffer = buffer })
+                            vim.keymap.set( "n", "<leader>cpm", rt.parent_module.parent_module, { desc = "Parent Module", buffer = buffer })
+                            vim.keymap.set( "n", "<leader>cmd", "<cmd>RustMoveItemDown<cr>", { desc = "Move Item Down", buffer = buffer })
+                            vim.keymap.set( "n", "<leader>cmu", "<cmd>RustMoveItemUp<cr>", { desc = "Move Item Up", buffer = buffer })
+                            vim.keymap.set( "n", "<leader>cjl", rt.join_lines.join_lines, { desc = "Join Lines", buffer = buffer })
+                            vim.keymap.set("n", "<leader>dr", "<cmd>RustDebuggables<cr>", { buffer = buffer, desc = "Run Debuggables (Rust)" })
+							-- stylua: ignore end
+						end
+					end)
+					local mason_registry = require("mason-registry")
+					-- rust tools configuration for debugging support
+					local codelldb = mason_registry.get_package("codelldb")
+					local extension_path = codelldb:get_install_path() .. "/extension/"
+					local codelldb_path = extension_path .. "adapter/codelldb"
+					local liblldb_path = vim.fn.has("mac") == 1 and extension_path .. "lldb/lib/liblldb.dylib"
+						or extension_path .. "lldb/lib/liblldb.so"
 
-				local rt = require("rust-tools")
-				local rust_opts = {
-					server = {
-						settings = {
-							["rust-analyzer"] = {
-								cargo = {
-									allFeatures = true,
-									loadOutDirsFromCheck = true,
-									runBuildScripts = true,
-								},
-								checkOnSave = {
-									allFeatures = true,
-									command = "clippy",
-									extraArgs = { "--no-deps" },
-								},
-								procMacro = {
-									enable = true,
-									ignored = {
-										["async-trait"] = { "async_trait" },
-										["napi-derive"] = { "napi" },
-										["async-recursion"] = { "async_recursion" },
+					local rust_opts = {
+						dap = {
+							adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path),
+						},
+						tools = {
+							on_initialized = function()
+								vim.cmd([[
+									augroup RustLSP
+										autocmd CursorHold                      *.rs silent! lua vim.lsp.buf.document_highlight()
+										autocmd CursorMoved,InsertEnter         *.rs silent! lua vim.lsp.buf.clear_references()
+										autocmd BufEnter,CursorHold,InsertLeave *.rs silent! lua vim.lsp.codelens.refresh()
+									augroup END
+								]])
+							end,
+						},
+						server = {
+							settings = {
+								["rust-analyzer"] = {
+									cargo = {
+										allFeatures = true,
+										loadOutDirsFromCheck = true,
+										runBuildScripts = true,
+									},
+									checkOnSave = {
+										allFeatures = true,
+										command = "clippy",
+										extraArgs = { "--no-deps" },
+									},
+									procMacro = {
+										enable = true,
+										ignored = {
+											["async-trait"] = { "async_trait" },
+											["napi-derive"] = { "napi" },
+											["async-recursion"] = { "async_recursion" },
+										},
 									},
 								},
 							},
 						},
-					},
-				}
-				local rt_opts = vim.tbl_deep_extend("force", opts, rust_opts)
-				rt.setup(rt_opts)
-				return true
-			end,
+					}
+					local rt_opts = vim.tbl_deep_extend("force", opts, rust_opts)
+					require("rust-tools").setup(rt_opts)
+					return true
+				end,
+			},
 		},
 	},
 }

89a3005b4020667ffc732ce5b96e7c151f3d7ff7
acehinnnqru <acehinnnqru@gmail.com>
2023-03-23 23:47:41 +0800

    feat(wezterm): rewrite configs

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,24 +1,38 @@
 local wezterm = require("wezterm")
+local configs = {}
 
-return {
-	color_scheme = "Catppuccin Macchiato",
-	font = wezterm.font_with_fallback({
-		{
-			family = "Consolas",
-		},
-
-		{
-			family = "Hack Nerd Font Mono",
-		},
-
-		{
-			family = "PingFang SC",
-		},
-	}),
-	font_size = 14.5,
-
-	window_decorations = "RESIZE",
-
-	hide_tab_bar_if_only_one_tab = true,
-	window_background_opacity = 0.85,
+-- appearance options
+-- color_scheme
+configs.color_scheme = "Catppuccin Macchiato"
+-- window padding
+configs.window_padding = {
+	left = 10,
+	right = 10,
+	top = 10,
+	bottom = 10,
 }
+
+-- opacity
+configs.window_background_opacity = 0.80
+
+-- tab bar
+configs.window_decorations = "RESIZE"
+configs.hide_tab_bar_if_only_one_tab = true
+
+-- font options
+configs.font = wezterm.font_with_fallback({
+	{
+		family = "Consolas",
+	},
+
+	{
+		family = "Hack Nerd Font Mono",
+	},
+
+	{
+		family = "PingFang SC",
+	},
+})
+configs.font_size = 15.5
+
+return configs

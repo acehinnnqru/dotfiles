4c7b88a93d8784935521d559ac8f4b386fce6908
acehinnnqru <acehinnnqru@gmail.com>
2023-03-24 22:04:52 +0800

    feat(nvim): add core module and init basic options

---
+++ .config/nvim/lua/au/core/options.lua
@@ -0,0 +1,38 @@
+return {
+    -- enable mouse
+    mouse = "a",
+
+    -- integrated with clipboard
+    -- unix
+    clipboard = "unnamedplus",
+
+    -- search options
+    hlsearch = true,
+    ignorecase = true,
+    smartcase = true,
+
+    -- split view options
+    splitbelow = true,
+    splitright = true,
+
+    -- tabline options
+    showtabline = 1,
+
+    -- pop menu, aka completion menu
+    pumheight = 10,
+    completeopt = { "menu", "menuone" },
+
+    -- file options
+    fileencoding = "utf-8",
+
+    -- enable undofile
+    undofile = true,
+
+    -- editor behaviors
+    joinspaces = false,
+    writebackup = false,
+
+    -- gutter options
+    number = true,
+    relativenumber = true,
+}

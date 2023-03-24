757f47fe9c0a1d7099f175b1bf9d1673ab069f02
acehinnnqru <acehinnnqru@gmail.com>
2023-03-24 23:06:38 +0800

    feat(nvim): add core keymaps

--- .config/nvim/lua/au/utils.lua
+++ .config/nvim/lua/au/utils.lua
@@ -7,4 +7,27 @@ function M.set_vim_opts(opts)
     end
 end
 
+local keymap_set = vim.keymap.set
+---
+-- Sets a list of keymaps using `vim.keymap.set`.
+--
+-- @function set_keymaps
+-- @tparam { {string, string, string} | {string, string, string, table} }[] keymaps A list of keymaps to set, where each keymap is a table of the form {mode, lhs, rhs} or {mode, lhs, rhs, opts}
+-- @tparam[opt] table opts Optional default options for all keymaps
+-- @usage set_keymaps({{'n', '<leader>f', ':Telescope find_files<CR>'}, {'n', '<leader>g', ':Telescope live_grep<CR>'}}, {noremap=true, silent=true})
+--
+function M.set_keymaps(keymaps, opts)
+  for _, map in ipairs(keymaps) do
+    local mode, lhs, rhs, options
+    if #map == 3 then
+      mode, lhs, rhs = unpack(map)
+    elseif #map == 4 then
+      mode, lhs, rhs, options = unpack(map)
+    else
+      error("Invalid keymap: " .. vim.inspect(map))
+    end
+    keymap_set(mode, lhs, rhs, vim.tbl_extend("force", options or {}, opts or {}))
+  end
+end
+
 return M

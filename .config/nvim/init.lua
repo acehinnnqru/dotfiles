1cc07037cbd62a55297dc0a6a02add77e75b5f2d
acehinnnqru <acehinnnqru@gmail.com>
2023-03-24 18:24:50 +0800

    feat(nvim): add lazy.nvim

---
+++ .config/nvim/init.lua
@@ -0,0 +1,28 @@
+-- install lazy
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
+end
+vim.opt.rtp:prepend(lazypath)
+
+local lazy_specs = {}
+local lazy_options = {
+    install = {
+        missing = true,
+	colorscheme = {},
+    },
+    change_detection = {
+        enabled = false,
+	notify = false,
+    }
+}
+
+-- setup lazy
+require("lazy").setup(plugins, opts)

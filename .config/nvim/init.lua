a0c4788d73f380324aa29601d27251601c9d1022
acehinnnqru <acehinnnqru@gmail.com>
2023-03-25 11:41:24 +0800

    feat(nvim): add minimal and extends; add coloscheme

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -14,18 +14,25 @@ vim.opt.rtp:prepend(lazypath)
 
 local env = require("au.env")
 
-local lazy_specs = {}
+local function get_lazy_spec(specs)
+    local s = {}
+    for k, v in ipairs(specs) do
+        s[k] = { import = v }
+    end
+    return s
+end
 
+local lazy_specs = {}
 if env.minimal then
-    lazy_specs = {}
+    lazy_specs.spec = get_lazy_spec({ "au.plugins.minimal", })
 else
     local e = env.environment
     if e == "nvim" then
-        lazy_specs = {}
+        lazy_specs.spec = get_lazy_spec({ "au.plugins.minimal", "au.plugins.extends" })
     elseif e == "vscode" then
-        lazy_specs = {}
+        lazy_specs.spec = {}
     elseif e == "gui" then
-        lazy_specs = {}
+        lazy_specs.spec = {}
     end
 end
 

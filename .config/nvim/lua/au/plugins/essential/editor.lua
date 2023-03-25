fd3c6f181d9d2cc2189fad950b78322bd932834d
acehinnnqru <acehinnnqru@gmail.com>
2023-03-26 02:29:15 +0800

    feat(nvim): add surround

--- .config/nvim/lua/au/plugins/essential/editor.lua
+++ .config/nvim/lua/au/plugins/essential/editor.lua
@@ -1,6 +1,14 @@
 return {
+    -- auto pairs
     {
 		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		config = true,
+    },
+
+    -- surround
+    {
+		"kylechui/nvim-surround",
 		event = "InsertEnter",
 		config = true,
     },

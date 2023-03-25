c4c5141e7f6fe2dcc382e8f3bc57ec5a614537c0
acehinnnqru <acehinnnqru@gmail.com>
2023-03-26 02:34:20 +0800

    feat(nvim): add mini.comment and ufo

--- .config/nvim/lua/au/plugins/pro/editor.lua
+++ .config/nvim/lua/au/plugins/pro/editor.lua
@@ -12,4 +12,27 @@ return {
 			{ "<leader>jp", "<cmd>HopPattern<cr>", desc = "Jump to Pattern" },
 		},
 	},
+
+    -- better fold
+    {
+		"kevinhwang91/nvim-ufo",
+		event = "VeryLazy",
+		dependencies = { "kevinhwang91/promise-async" },
+		opts = {
+			open_fold_hl_timeout = 0,
+			provider_selector = function(_, _, _)
+                return { "indent" }
+			end,
+		},
+		config = function(_, opts)
+			vim.o.foldcolumn = "0"
+
+			-- using ufo provider need a large value, feel free to decrease the value
+			vim.o.foldlevel = 99
+			vim.o.foldlevelstart = 99
+			vim.o.foldenable = true
+
+			require("ufo").setup(opts)
+		end,
+	},
 }

ab50dca41721392871e8da4526ec0b7dca66fa2c
acehinnnqru <acehinnnqru@gmail.com>
2023-03-26 16:51:07 +0800

    feat(nvim): add indent hl plugin

--- .config/nvim/lua/au/plugins/essential/editor.lua
+++ .config/nvim/lua/au/plugins/essential/editor.lua
@@ -19,4 +19,17 @@ return {
 		keys = { "gc", "gcc" },
 		config = true,
     },
+
+    -- indent
+    -- support context, line start
+    {
+		"lukas-reineke/indent-blankline.nvim",
+		event = "VeryLazy",
+		opts = {
+			filetype_exclude = { "help", "alpha", "dashboard", "lazy" },
+			show_trailing_blankline_indent = false,
+			show_end_of_line = false,
+			space_char_blankline = " ",
+		},
+	},
 }

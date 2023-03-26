d212a44fe3d53571abd4dd425e763a110dcf6468
acehinnnqru <acehinnnqru@gmail.com>
2023-03-26 19:04:28 +0800

    feat(nvim): add neo tree support

--- .config/nvim/lua/au/plugins/essential/editor.lua
+++ .config/nvim/lua/au/plugins/essential/editor.lua
@@ -35,4 +35,55 @@ return {
 			space_char_blankline = " ",
 		},
 	},
+
+    -- neo tree instead of original netrw
+    {
+		"nvim-neo-tree/neo-tree.nvim",
+		dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+        branch = "v2.x",
+		cmd = "Neotree",
+		keys = {
+			{
+				"<leader>fe",
+				function()
+					require("neo-tree.command").execute({ toggle = true, dir = require("au.utils").get_root() })
+				end,
+				desc = "Explorer NeoTree (root dir)",
+			},
+			{
+				"<leader>fE",
+				function()
+					require("neo-tree.command").execute({ toggle = true, dir = vim.loop.cwd() })
+				end,
+				desc = "Explorer NeoTree (cwd)",
+			},
+			{ "<leader>e", "<leader>fe", desc = "Explorer NeoTree (root dir)", remap = true },
+			{ "<leader>E", "<leader>fE", desc = "Explorer NeoTree (cwd)", remap = true },
+		},
+		init = function()
+			vim.g.neo_tree_remove_legacy_commands = 1
+            -- override the netrw when opening a dir
+			if vim.fn.argc() == 1 then
+				local stat = vim.loop.fs_stat(vim.fn.argv(0))
+				if stat and stat.type == "directory" then
+					require("neo-tree")
+				end
+			end
+		end,
+		opts = {
+			filesystem = {
+				filtered_items = {
+					hide_dotfiles = false,
+					hide_gitignored = false,
+				},
+				bind_to_cwd = false,
+				follow_current_file = true,
+			},
+			window = {
+				mappings = {
+					["<space>"] = "none",
+				},
+			},
+		},
+	},
 }

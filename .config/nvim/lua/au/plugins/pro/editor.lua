8be099c801240d4e02a0634ff8579116076d93a9
acehinnnqru <acehinnnqru@gmail.com>
2023-03-26 17:58:24 +0800

    feat(nvim): add gitsigns

--- .config/nvim/lua/au/plugins/pro/editor.lua
+++ .config/nvim/lua/au/plugins/pro/editor.lua
@@ -76,4 +76,37 @@ return {
 		},
 		config = true,
 	},
+
+    -- git signs and jump
+    {
+		"lewis6991/gitsigns.nvim",
+		event = "VeryLazy",
+		opts = {
+			current_line_blame = true,
+			current_line_blame_opts = {
+				delay = 100,
+			},
+			on_attach = function(buffer)
+				local gs = package.loaded.gitsigns
+                require("au.utils").set_keymaps({
+                    { "n", "]h", gs.next_hunk, { desc = "Next Hunk" }},
+                    { "n", "[h", gs.prev_hunk, { desc = "Prev Hunk" }},
+                    { { "n", "v" }, "<leader>ghs", ":Gitsigns stage_hunk<CR>", {  desc = "Stage Hunk" }},
+                    { { "n", "v" }, "<leader>ghr", ":Gitsigns reset_hunk<CR>", {  desc = "Reset Hunk" }},
+                    { "n", "<leader>ghS", gs.stage_buffer, { desc = "Stage Buffer" }},
+                    { "n", "<leader>ghu", gs.undo_stage_hunk, {desc = "Undo Stage Hunk" }},
+                    { "n", "<leader>ghR", gs.reset_buffer, { desc = "Reset Buffer" }},
+                    { "n", "<leader>ghp", gs.preview_hunk, { desc = "Preview Hunk" }},
+                    { "n", "<leader>ghb", function()
+                        gs.blame_line({ full = true })
+                    end, { desc = "Blame Line" }},
+                    { "n", "<leader>ghd", gs.diffthis, { desc = "Diff This" }},
+                    { "n", "<leader>ghD", function()
+                        gs.diffthis("~")
+                    end, {desc = "Diff This ~" }},
+                    { { "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", {desc = "GitSigns Select Hunk" }},
+                }, { buffer = buffer })
+			end,
+		},
+	},
 }

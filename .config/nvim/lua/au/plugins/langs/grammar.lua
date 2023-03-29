70964a34fa66bd6a29fe42f75fffdb4be8e1914c
acehinnnqru <acehinnnqru@gmail.com>
2023-03-30 00:14:05 +0800

    feat(nvim): update grammar lua

--- .config/nvim/lua/au/plugins/langs/grammar.lua
+++ .config/nvim/lua/au/plugins/langs/grammar.lua
@@ -2,7 +2,7 @@ return {
     {
         "jay-babu/mason-null-ls.nvim",
         opts = function(_, opts)
-            vim.list_extend(opts.ensure_installed, { "cspell", "alex", "proselint", "vale" })
+            vim.list_extend(opts.ensure_installed, { "alex", "proselint", "misspell", "vale" })
         end,
     },
 
@@ -10,12 +10,10 @@ return {
         "jose-elias-alvarez/null-ls.nvim",
         opts = function(_, opts)
             local nls = require("null-ls")
-            table.insert(opts.sources, nls.builtins.diagnostics.cspell)
-            table.insert(opts.sources, nls.builtins.code_actions.cspell)
-
             table.insert(opts.sources, nls.builtins.diagnostics.alex)
-            table.insert(opts.sources, nls.builtins.diagnostics.proselint)
+            table.insert(opts.sources, nls.builtins.code_actions.proselint)
             table.insert(opts.sources, nls.builtins.diagnostics.vale)
+            table.insert(opts.sources, nls.builtins.diagnostics.misspell)
             return opts
         end,
     },

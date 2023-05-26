22e53b99a7c61c4be24919a9930c210ed15767ff
acehinnnqru <acehinnnqru@gmail.com>
2023-05-26 14:34:45 +0800

    feat(nvim): set neovim python host prog

--- .config/nvim/lua/au/plugins/langs/python.lua
+++ .config/nvim/lua/au/plugins/langs/python.lua
@@ -1,7 +1,7 @@
 -- set some global options
 require("au.utils").set_g_opts({
-    python_host_prog = "/usr/bin/python3",
-    python3_host_prog = "/usr/bin/python3",
+    python_host_prog = "/Users/acehinnnqru/.pyenv/versions/neovim/bin/python",
+    python3_host_prog = "/Users/acehinnnqru/.pyenv/versions/neovim/bin/python3",
 })
 
 return {

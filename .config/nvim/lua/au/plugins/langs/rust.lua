07cddcf19e4082a75d3b5d1eef3f4fe98fdb4c77
acehinnnqru <acehinnnqru@gmail.com>
2023-09-18 23:14:00 +0800

    feat(nvim): remove dap support for rust

--- .config/nvim/lua/au/plugins/langs/rust.lua
+++ .config/nvim/lua/au/plugins/langs/rust.lua
@@ -58,22 +58,10 @@ return {
                             vim.keymap.set( "n", "<leader>cmd", "<cmd>RustMoveItemDown<cr>", { desc = "Move Item Down", buffer = buffer })
                             vim.keymap.set( "n", "<leader>cmu", "<cmd>RustMoveItemUp<cr>", { desc = "Move Item Up", buffer = buffer })
                             vim.keymap.set( "n", "<leader>cjl", rt.join_lines.join_lines, { desc = "Join Lines", buffer = buffer })
-                            vim.keymap.set("n", "<leader>dr", "<cmd>RustDebuggables<cr>", { buffer = buffer, desc = "Run Debuggables (Rust)" })
                             -- stylua: ignore end
                         end
                     end)
-                    local mason_registry = require("mason-registry")
-                    -- rust tools configuration for debugging support
-                    local codelldb = mason_registry.get_package("codelldb")
-                    local extension_path = codelldb:get_install_path() .. "/extension/"
-                    local codelldb_path = extension_path .. "adapter/codelldb"
-                    local liblldb_path = vim.fn.has("mac") == 1 and extension_path .. "lldb/lib/liblldb.dylib"
-                        or extension_path .. "lldb/lib/liblldb.so"
-
                     local rust_opts = {
-                        dap = {
-                            adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path),
-                        },
                         tools = {
                             on_initialized = function()
                                 vim.cmd([[
@@ -96,10 +84,10 @@ return {
                                         loadOutDirsFromCheck = true,
                                         runBuildScripts = true,
                                     },
-                                    checkOnSave = {
-                                        allFeatures = true,
+                                    checkOnSave = true,
+                                    check = {
+                                        allTargets = true,
                                         command = "clippy",
-                                        extraArgs = { "--no-deps" },
                                     },
                                     procMacro = {
                                         enable = true,

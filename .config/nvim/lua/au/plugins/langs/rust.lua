f9ea8545c3be41bc76c8da200e4c13c661ae8d70
acehinnnqru <acehinnnqru@gmail.com>
2023-11-11 15:43:19 +0800

    feat(nvim): update rust config

--- .config/nvim/lua/au/plugins/langs/rust.lua
+++ .config/nvim/lua/au/plugins/langs/rust.lua
@@ -40,72 +40,51 @@ return {
     },
 
     {
-        "neovim/nvim-lspconfig",
-        dependencies = { "simrat39/rust-tools.nvim" },
-        opts = {
-            servers = {
-                rust_analyzer = {},
-            },
-            setup = {
-                rust_analyzer = function(_, opts)
-                    require("au.utils").lsp_on_attach(function(client, buffer)
-                        if client.name == "rust_analyzer" then
-                            local rt = require("rust-tools")
-                            -- stylua: ignore start
-                            vim.keymap.set( "n", "K", "<cmd>RustHoverActions<cr>", { desc = "Hover Action", buffer = buffer })
-                            vim.keymap.set( "n", "<leader>cem", rt.expand_macro.expand_macro, { desc = "Expand Macro", buffer = buffer })
-                            vim.keymap.set( "n", "<leader>cpm", rt.parent_module.parent_module, { desc = "Parent Module", buffer = buffer })
-                            vim.keymap.set( "n", "<leader>cmd", "<cmd>RustMoveItemDown<cr>", { desc = "Move Item Down", buffer = buffer })
-                            vim.keymap.set( "n", "<leader>cmu", "<cmd>RustMoveItemUp<cr>", { desc = "Move Item Up", buffer = buffer })
-                            vim.keymap.set( "n", "<leader>cjl", rt.join_lines.join_lines, { desc = "Join Lines", buffer = buffer })
-                            -- stylua: ignore end
-                        end
-                    end)
-                    local rust_opts = {
-                        tools = {
-                            on_initialized = function()
-                                vim.cmd([[
-									augroup RustLSP
-										autocmd CursorHold                      *.rs silent! lua vim.lsp.buf.document_highlight()
-										autocmd CursorMoved,InsertEnter         *.rs silent! lua vim.lsp.buf.clear_references()
-										autocmd BufEnter,CursorHold,InsertLeave *.rs silent! lua vim.lsp.codelens.refresh()
-									augroup END
-								]])
-                            end,
-                            inlay_hints = {
-                                auto = false,
+        "mrcjkb/rustaceanvim",
+        version = "v3",
+        ft = { "rust" },
+        command = "RustLsp",
+        config = function()
+            vim.g.rustaceanvim = {
+                tools = {
+                    on_initialized = function()
+                        vim.cmd([[
+                            augroup RustLSP
+                            autocmd CursorHold                      *.rs silent! lua vim.lsp.buf.document_highlight()
+                            autocmd CursorMoved,InsertEnter         *.rs silent! lua vim.lsp.buf.clear_references()
+                            autocmd BufEnter,CursorHold,InsertLeave *.rs silent! lua vim.lsp.codelens.refresh()
+                            augroup END
+                            ]])
+                    end,
+                    inlay_hints = {
+                        auto = false,
+                    },
+                },
+                server = {
+                    settings = {
+                        ["rust-analyzer"] = {
+                            cargo = {
+                                allFeatures = true,
+                                loadOutDirsFromCheck = true,
+                                runBuildScripts = true,
                             },
-                        },
-                        server = {
-                            settings = {
-                                ["rust-analyzer"] = {
-                                    cargo = {
-                                        allFeatures = true,
-                                        loadOutDirsFromCheck = true,
-                                        runBuildScripts = true,
-                                    },
-                                    checkOnSave = true,
-                                    check = {
-                                        allTargets = true,
-                                        command = "clippy",
-                                    },
-                                    procMacro = {
-                                        enable = true,
-                                        ignored = {
-                                            ["async-trait"] = { "async_trait" },
-                                            ["napi-derive"] = { "napi" },
-                                            ["async-recursion"] = { "async_recursion" },
-                                        },
-                                    },
+                            checkOnSave = true,
+                            check = {
+                                enable = true,
+                                command = "clippy",
+                            },
+                            procMacro = {
+                                enable = true,
+                                ignored = {
+                                    ["async-trait"] = { "async_trait" },
+                                    ["napi-derive"] = { "napi" },
+                                    ["async-recursion"] = { "async_recursion" },
                                 },
                             },
                         },
-                    }
-                    local rt_opts = vim.tbl_deep_extend("force", opts, rust_opts)
-                    require("rust-tools").setup(rt_opts)
-                    return true
-                end,
-            },
-        },
+                    },
+                },
+            }
+        end,
     },
 }

7b1667927c8bff26e62e39b3ba46d526ff211d2d
acehinnnqru <acehinnnqru@gmail.com>
2026-06-13 21:00:32 +0800

    chore: remove redundant code

--- config/nvim/lua/au/plugins/langs/sql.lua
+++ config/nvim/lua/au/plugins/langs/sql.lua
@@ -15,9 +15,6 @@ return {
         opts = function(_, opts)
             local nls = require("null-ls")
             table.insert(opts.sources, nls.builtins.formatting.sqlfluff)
-            nls.builtins.formatting.sqlfluff.with({
-                extra_args = { "--dialect", "postgres" }, -- change to your dialect
-            })
         end,
     },
     {

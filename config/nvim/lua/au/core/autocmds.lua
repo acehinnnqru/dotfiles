180edb8ecb8b7f3702243bc0b211cdd9fcb7861e
acehinnnqru <acehinnnqru@gmail.com>
2026-06-30 10:10:38 +0800

    fix(nvim): don't set modifiable

--- config/nvim/lua/au/core/autocmds.lua
+++ config/nvim/lua/au/core/autocmds.lua
@@ -1,8 +1,24 @@
 vim.api.nvim_create_autocmd("BufReadPost", {
     pattern = "*",
     callback = function()
+        local bufname = vim.fn.bufname()
+        if bufname == "" then
+            return
+        end
+        if vim.bo.buftype ~= "" then
+            return
+        end
+        if vim.bo.filetype == "fzf" then
+            return
+        end
+        if vim.fn.filereadable(bufname) == 0 then
+            return
+        end
+
         if vim.bo.readonly then
-            vim.bo.modifiable = false
+            vim.keymap.set("n", "o", "<Nop>", { buffer = true })
+            vim.keymap.set("n", "i", "<Nop>", { buffer = true })
+            vim.keymap.set("n", "a", "<Nop>", { buffer = true })
         end
     end,
 })

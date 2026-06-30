9810d5cb4c459a0757eb48de6c734dc803759cbd
acehinnnqru <acehinnnqru@gmail.com>
2026-06-30 10:06:09 +0800

    feat(nvim): add is_darwin and is_linux utils

--- config/nvim/lua/au/utils.lua
+++ config/nvim/lua/au/utils.lua
@@ -184,4 +184,13 @@ function M.install_ts(langs)
     end
 end
 
+---@return boolean
+function M.is_darwin()
+    return vim.loop.os_uname().sysname == "Darwin"
+end
+
+function M.is_linux()
+    return vim.loop.os_uname().sysname == "Linux"
+end
+
 return M
